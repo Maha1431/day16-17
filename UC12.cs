d931efddd14479e6c078ef513db523b9a2a48630
Maha <91832344+maha239@users.noreply.github.com>
2021-11-15 18:18:43 +0530

    first commit

---
+++ UC12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace datastructure_and_algorithm
+{
+    class UC12
+    {
+        public static void Regex1()
+        {
+            string input = "Hello <<name>>,name as << full name >> in our system.\nyour contact number is 91 - xxxxxxxxxx.\nPlease, let us know in case of any clarification Thank you BridgeLabz 01 / 01 / 2016.";
+            Console.WriteLine(input);
+
+            input = Regex.Replace(input, @"<<name>>", "Maha");
+            input = Regex.Replace(input, @"<< full name >>", "MahaLakshmi");
+            input = Regex.Replace(input, @"1234567899", "");
+            input = Regex.Replace(input, @"01 / 11 / 2018", "01 / 11/ 2021");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Replace Details Using Regex");
+            Console.WriteLine("\n");
+            Console.WriteLine(input);
+        }
+    }
+}
